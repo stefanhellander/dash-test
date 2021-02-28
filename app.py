2acc5cbd7cd31205bf0bffebf842d0e0455899db
Stefan Hellander <stefan@scaleoutsystems.com>
2021-02-28 17:33:16 +0100

    dash update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    html.H6("Redeploy with Git push!"),
+    html.H6("Basic Dash example."),
     html.Div(["Input: ",
               dcc.Input(id='my-input', value='initial value', type='text')]),
     html.Br(),
