d7e64899be065a85e3fdc8cf6c70674576f51a5d
Stefan Hellander <stefan@scaleoutsystems.com>
2021-02-28 18:33:49 +0100

    ...

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.layout = html.Div([
-    html.H6("Git update 7."),
+    html.H6("Git update 8."),
     html.Div(["Input: ",
               dcc.Input(id='my-input', value='initial value', type='text')]),
     html.Br(),
